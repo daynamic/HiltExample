408c75714119f7c07cef1bbcd8759bae9bebb48f
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-09-17 11:02:59 -0400

    Usage of field injection

--- app/src/main/java/com/akshat/hiltexample/MainActivity.kt
+++ app/src/main/java/com/akshat/hiltexample/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.akshat.hiltexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akshat.hiltexample.database.DatabaseAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var databaseAdapter: DatabaseAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d(TAG, "DatabaseAdapter : $databaseAdapter")
+        databaseAdapter.log("Hello Hilt")
     }
 }
