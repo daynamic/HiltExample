7f0da96eb36ebc94389d2d5515f388d480bd3f81
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-09-17 11:53:17 -0400

    Inject interfaces with @Binds

--- app/src/main/java/com/akshat/hiltexample/MainActivity.kt
+++ app/src/main/java/com/akshat/hiltexample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.akshat.hiltexample.database.DatabaseAdapter
 import com.akshat.hiltexample.database.DatabaseService
+import com.akshat.hiltexample.network.NetworkAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -14,12 +15,17 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var databaseAdapter: DatabaseAdapter
 
+    @Inject
+    lateinit var networkAdapter: NetworkAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "DatabaseAdapter : $databaseAdapter")
         databaseAdapter.log("Hello Hilt")
+
+        networkAdapter.log("Interface Binding")
     }
 
     @Inject
--- app/src/main/java/com/akshat/hiltexample/hilt/NetworkModule.kt
+++ app/src/main/java/com/akshat/hiltexample/hilt/NetworkModule.kt
@@ -1,10 +1,16 @@
 package com.akshat.hiltexample.hilt
 
+import com.akshat.hiltexample.network.MyAppNetworkAdapter
+import com.akshat.hiltexample.network.NetworkAdapter
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 
 @Module
 @InstallIn(ActivityComponent::class)
-class NetworkModule {
+abstract class NetworkModule {
+
+    @Binds
+    abstract fun bindNetworkAdapterImpl(networkAdapterImpl: MyAppNetworkAdapter): NetworkAdapter
 }
