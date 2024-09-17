80aabd5e61cd6eaac11c33ee933efb2a8b3e4ca1
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-09-17 12:17:59 -0400

    providing context in Hilt

--- app/src/main/java/com/akshat/hiltexample/database/DatabaseAdapter.kt
+++ app/src/main/java/com/akshat/hiltexample/database/DatabaseAdapter.kt
@@ -1,13 +1,18 @@
 package com.akshat.hiltexample.database
 
+import android.content.Context
 import android.util.Log
 import com.akshat.hiltexample.TAG
+import dagger.hilt.android.qualifiers.ActivityContext
 import javax.inject.Inject
 
-class DatabaseAdapter @Inject constructor(var databaseService: DatabaseService) {
+class DatabaseAdapter @Inject constructor(
+    @ActivityContext private val context: Context,
+    var databaseService: DatabaseService) {
  fun log(message : String){
      Log.d(TAG, "DatabaseAdapter : $message" )
      databaseService.log(message)
+     Log.d(TAG, "Context : $context" )
  }
 
 }
