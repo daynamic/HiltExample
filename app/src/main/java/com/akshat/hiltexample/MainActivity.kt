deef333a9d3187bec03614f58fa6f861e9a7b022
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-09-17 11:10:38 -0400

    Method injection

--- app/src/main/java/com/akshat/hiltexample/MainActivity.kt
+++ app/src/main/java/com/akshat/hiltexample/MainActivity.kt
@@ -2,18 +2,17 @@ package com.akshat.hiltexample
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.akshat.hiltexample.database.DatabaseAdapter
+import com.akshat.hiltexample.database.DatabaseService
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject lateinit var databaseAdapter: DatabaseAdapter
+    @Inject
+    lateinit var databaseAdapter: DatabaseAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG, "DatabaseAdapter : $databaseAdapter")
         databaseAdapter.log("Hello Hilt")
+    }
+
+    @Inject
+    fun directToDatabase(databaseService: DatabaseService) {
+        databaseService.log("Method injection")
     }
 }
